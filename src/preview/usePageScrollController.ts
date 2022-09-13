d4cc12ad7783cd67b5a44e167d256137575b3a24
Beatrice Guerra <beatrice.guerra@zextras.com>
2022-09-13 14:35:29 +0200

    fix: recreate page observer on resize and increase intersection area
    refs: PREV-66

--- src/preview/usePageScrollController.ts
+++ src/preview/usePageScrollController.ts
@@ -13,14 +13,22 @@ export function usePageScrollController(
 } {
 	const updatePageObserverRef = useRef<IntersectionObserver>();
 	const currentPageIntersectionEntryRef = useRef<IntersectionObserverEntry>();
+	const observedPagesRef = useRef<HTMLElement[]>([]);
 
-	useEffect(() => {
+	const destroyObserver = useCallback(() => {
+		updatePageObserverRef.current && updatePageObserverRef.current.disconnect();
+	}, []);
+
+	const initObserver = useCallback(() => {
+		// destroy before creating a new one
+		destroyObserver();
+
 		if (scrollContainerRef.current) {
 			const { clientHeight } = scrollContainerRef.current;
 			const options: IntersectionObserverInit = {
 				root: scrollContainerRef.current,
 				// set root margin to be a line at ~30% of the scrollable area visible height
-				rootMargin: `-${clientHeight * 0.3}px 0px -${clientHeight * 0.7}px 0px`,
+				rootMargin: `-${clientHeight * 0.3}px 0px -${clientHeight * 0.65}px 0px`,
 				threshold: 0
 			};
 			updatePageObserverRef.current = new IntersectionObserver((entries) => {
@@ -32,15 +40,37 @@ export function usePageScrollController(
 				onPageChange(currentPageIntersectionEntryRef.current?.target);
 			}, options);
 		}
+	}, [destroyObserver, onPageChange, scrollContainerRef]);
+
+	const reObserveAll = useCallback(() => {
+		initObserver();
+		observedPagesRef.current.forEach((observedPage) => {
+			if (updatePageObserverRef.current) {
+				updatePageObserverRef.current.observe(observedPage);
+			}
+		});
+	}, [initObserver]);
+
+	useEffect(() => {
+		window.addEventListener('resize', reObserveAll);
+		// first run
+		initObserver();
 
 		return (): void => {
-			updatePageObserverRef.current && updatePageObserverRef.current.disconnect();
+			// on unmount
+			// destroy observer
+			destroyObserver();
+			// unregister listener
+			window.removeEventListener('resize', reObserveAll);
+			// cleanup observed pages
+			observedPagesRef.current = [];
 		};
-	}, [onPageChange, scrollContainerRef]);
+	}, [initObserver, reObserveAll, destroyObserver]);
 
 	const observePage = useCallback((pageElement: HTMLElement) => {
 		if (updatePageObserverRef.current) {
 			updatePageObserverRef.current.observe(pageElement);
+			observedPagesRef.current.push(pageElement);
 		}
 	}, []);
 
