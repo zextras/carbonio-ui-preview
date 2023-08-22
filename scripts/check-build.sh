689c8719b9235fa410a3eae1a6d0739d7ebe5b65
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-08-22 16:16:04 +0200

    fix(build): run check of the build only on pre-push
    Avoid running check in ci since environment is different

--- scripts/check-build.sh
+++ scripts/check-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -f ../lib/index.js ] || [ ! -f ../lib-esm/index.js ]; then
+if [ ! -f ./lib/index.js ] || [ ! -f ./lib-esm/index.js ]; then
     echo "Error: build folders are not well structured"
     echo "index.js is not directly contained inside build package";
     exit 1;
