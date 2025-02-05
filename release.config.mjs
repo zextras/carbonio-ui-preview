4c50635767e1d09703121da336db4ca1ab007f7e
rodleyorosa <rodley.orosa@zextras.com>
2025-02-05 16:57:39 +0100

    chore: enable release for refactor and build commits

--- release.config.mjs
+++ release.config.mjs
@@ -7,31 +7,36 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-    branches: [
-        "release",
-        {
-            name: 'devel',
-            prerelease: true
-        },
-        {
-            name: 'next/+([0-9]).+([0-9]).+([0-9])',
-            prerelease: '${name.replace(/[\\/\\.]/g, "-")}'
-        }
-    ],
-    plugins: [
-        [
-            "@semantic-release/commit-analyzer",
-            {
-                preset: "conventionalcommits"
-            }
-        ],
-        [
-            "@semantic-release/release-notes-generator",
-            {
-                preset: "conventionalcommits"
-            }
-        ],
-        '@semantic-release/npm',
-        '@semantic-release/github'
-    ]
+	branches: [
+		'release',
+		{
+			name: 'devel',
+			prerelease: true
+		},
+		{
+			name: 'next/+([0-9]).+([0-9]).+([0-9])',
+			prerelease: '${name.replace(/[\\/\\.]/g, "-")}'
+		}
+	],
+	plugins: [
+		[
+			'@semantic-release/commit-analyzer',
+			{
+				preset: 'conventionalcommits',
+				releaseRules: [
+					// enable release also for refactor and build commits
+					{ type: 'refactor', release: 'patch' },
+					{ type: 'build', release: 'patch' }
+				]
+			}
+		],
+		[
+			'@semantic-release/release-notes-generator',
+			{
+				preset: 'conventionalcommits'
+			}
+		],
+		'@semantic-release/npm',
+		'@semantic-release/github'
+	]
 };
