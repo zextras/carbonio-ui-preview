bbe378b1f875c868da44e545e7f1364fec796026
Cataldo Mazzilli <cm@zextras.com>
2025-10-30 16:01:20 +0100

    test: wip

--- src/tests/vitest-setup.ts
+++ src/tests/vitest-setup.ts
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/vitest';
 import { vi } from 'vitest';
 import failOnConsole from 'vitest-fail-on-console';
 
